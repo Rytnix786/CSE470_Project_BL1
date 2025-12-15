ac49ef2ee82e2e7c40017a8326b345c9f95e8a86
Mehedi D Nafis <nafismehedi37@gmail.com>
2025-12-16 03:35:29 +0600

    Fix: Email config and health records validation - Complete working BRACU Consultation System

--- server/src/modules/healthRecords/healthRecords.validation.js
+++ server/src/modules/healthRecords/healthRecords.validation.js
@@ -3,12 +3,12 @@ const { z } = require('zod');
 const createHealthRecordSchema = z.object({
   date: z.string().optional(),
   bloodPressure: z.object({
-    systolic: z.number().optional(),
-    diastolic: z.number().optional(),
+    systolic: z.union([z.number(), z.string()]).optional().transform(val => val === '' || val === null || val === undefined ? undefined : Number(val)),
+    diastolic: z.union([z.number(), z.string()]).optional().transform(val => val === '' || val === null || val === undefined ? undefined : Number(val)),
   }).optional(),
-  bloodSugar: z.number().optional(),
-  weight: z.number().optional(),
-  height: z.number().optional(),
+  bloodSugar: z.union([z.number(), z.string()]).optional().transform(val => val === '' || val === null || val === undefined ? undefined : Number(val)),
+  weight: z.union([z.number(), z.string()]).optional().transform(val => val === '' || val === null || val === undefined ? undefined : Number(val)),
+  height: z.union([z.number(), z.string()]).optional().transform(val => val === '' || val === null || val === undefined ? undefined : Number(val)),
   notes: z.string().optional(),
 });
 
